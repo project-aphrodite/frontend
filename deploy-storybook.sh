cda26b83ad10f2189039f6d2de16a9bd9c73e708
Christiaan Landman <chriz.ek@gmail.com>
2021-04-20 20:58:17 +0200

    Attempting Storybook deployment.

---
+++ deploy-storybook.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run storybook:build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f https://github.com/project-aphrodite/front-end-storybook.git master:master-deploy
+
+cd -
